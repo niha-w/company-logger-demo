c055b02e5f5c4ebcd4b897e17b42af566d17a790
Niha-w <niharika.patel@arconnet.com>
2026-08-28 11:55:32 +0530

    v0.1

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from company_logger import logger
 
 logger.info(
     "user_logged_in",
-    user_id=12345,      
-    login_method="password"     
+    user_id=12345,
+    login_method="password",
 )
 
 
@@ -18,4 +18,9 @@ except Exception:
         order_id="ORD-1001",
     )
 
-# print("This is a developer print statement")
+logger.info(
+    "user_login",
+    username="niharika",
+    password="MyPassword123",
+    access_token="abc123-secret-token",
+)
--- company_logger/logger.py
+++ company_logger/logger.py
@@ -1,12 +1,49 @@
+import os
+
 import structlog
 
 
-SERVICE_NAME = "payment-service"
-ENVIRONMENT = "development"
+SERVICE_NAME = os.getenv(
+    "SERVICE_NAME",
+    "unknown-service",
+)
+
+ENVIRONMENT = os.getenv(
+    "ENVIRONMENT",
+    "development",
+)
+
+
+SENSITIVE_FIELDS = {
+    "password",
+    "passwd",
+    "pwd",
+    "access_token",
+    "refresh_token",
+    "api_key",
+    "apikey",
+    "secret",
+    "authorization",
+    "credit_card",
+    "card_number",
+}
+
+
+def redact_sensitive_data(logger, method_name, event_dict):
+    """
+    Replace sensitive values with [REDACTED].
+    """
+
+    for field in SENSITIVE_FIELDS:
+        if field in event_dict:
+            event_dict[field] = "[REDACTED]"
+
+    return event_dict
 
 
 structlog.configure(
     processors=[
+        redact_sensitive_data,
         structlog.processors.TimeStamper(fmt="iso", utc=True),
         structlog.processors.add_log_level,
         structlog.processors.format_exc_info,
